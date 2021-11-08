e1e6a063f9705823a379135b2aa455716fad0008
Swerik <derswerik@gmail.com>
2021-11-08 15:25:46 +0100

    add ExtensionMethodsTest

--- YouTubeStreamTemplates/src/Templates/ExtensionMethods.cs
+++ YouTubeStreamTemplates/src/Templates/ExtensionMethods.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using YouTubeStreamTemplates.Exceptions;
 
 namespace YouTubeStreamTemplates.Templates
 {
     public static class ExtensionMethods
     {
-        public static string GetValue(this IEnumerable<string> lines, string valueName)
+        public static string GetValue(this IEnumerable<string> lines, string key)
         {
-            var line = lines.FirstOrDefault(l => l.Trim().StartsWith(valueName + ":"));
-            if (line == null) throw new Exception("THERE IS NO " + valueName);
-            return line.Substring(valueName.Length + 2).Trim();
+            var line = lines.FirstOrDefault(l => l.Trim().StartsWith(key + ":"));
+            if (line == null) throw new LineMissingKeyException(key);
+            return line[(key.Length + 1)..].Trim();
         }
     }
 }
